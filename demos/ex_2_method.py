a4bcae7eda81cd17ff7880c6c0df073af4058bc0
Ray Mendoza <raymendozauci@gmail.com>
2022-03-13 17:41:45 -0700

    adding ex2

---
+++ demos/ex_2_method.py
@@ -0,0 +1,26 @@
+# Exercise 2: Methods
+
+# In this exercise, we will create your first method.
+# A method (sometimes called a function, or subroutine) is a group of 
+# statements we put together under a method name.
+# We can use this method name to execute all the statements
+# contained in this method.  An example is below:
+
+# Declares a new method called 'my_method'.  Method definitions are declared
+# using the 'def' keyword.
+def my_method():
+    # this is a list of statements.  Here we have only one for an example
+	print("We are printing from my_method!")
+
+# call the method we have created
+print("This is the first statement, after this we will call my_method.")
+my_method()
+
+
+# Exercise:
+
+# Remember, for every exercise you do, create a new branch to work from.
+
+# 1.) Create a method with two print statements in it.  Call the method "display_info".
+
+# 2.) Call the method twice.
